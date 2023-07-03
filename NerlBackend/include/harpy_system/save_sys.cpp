f3124d2857977884df9a7e240fb0506d0c43db34
дмитрий антипов <limbo.crow@mail.ru>
2023-07-03 15:52:30 +0300

    Initial commit

---
+++ NerlBackend/include/harpy_system/save_sys.cpp
@@ -0,0 +1,7 @@
+﻿#include "save_sys.h"
+
+bool harpy::system::is_blob(std::string str)
+{
+    if (str.find_last_of(".blob", str.size() - 6) == std::string::npos) return false;
+    return false;
+}
